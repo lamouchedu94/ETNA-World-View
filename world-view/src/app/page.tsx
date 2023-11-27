05a7d1d5c6cc568eb4d790a977bb4b84686cdc61
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-27 14:23:40 +0100

    [ADD] Research button

--- world-view/src/app/page.tsx
+++ world-view/src/app/page.tsx
@@ -3,17 +3,30 @@ import { useEffect, useState } from 'react'
 import { fetchUserData } from '@/sevices/users/UsersService'
 
 export default function Home() {
+  const [inputCountry, setinputContry] = useState<string>('')
   const [data, setData] = useState<any>()
-  useEffect(() => {
-    fetchUserData('France').then((response) => {
-      setData(response.data)
-    })
-  })
+
+  function changeCurrentCountry() {
+    if (inputCountry !== 'null') {
+      fetchUserData(inputCountry).then((response) => {
+        setData(response.data)
+      })
+    }
+  }
 
   return (
-    <div>
-      <p>hello world</p>
-      {data && <p>{data[0].name.common}</p>}
-    </div>
+    <body className="flex justify-center">
+      <main className="flex flex-col align-middle">
+        <p>hello world</p>
+        <input
+          type="text"
+          className="bg-[rgb(229,229,229)]"
+          value={inputCountry}
+          onChange={(v) => setinputContry(v.currentTarget.value)}
+        />
+        <button onClick={changeCurrentCountry}>Research</button>
+        {data && <p>{data[0].name.common}</p>}
+      </main>
+    </body>
   )
 }
