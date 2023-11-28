b7c5149e43c156ca27b7baa527f4f65a2c99e34e
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-28 18:57:16 +0100

    [ADD] Css border

--- world-view/src/components/country.tsx
+++ world-view/src/components/country.tsx
@@ -29,7 +29,7 @@ interface CarteProps {
 
 export function Carte({ data }: CarteProps) {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center border-2 rounded-lg p-3 mb-10">
       {data && <p>{data.name.common}</p>}
       {data && <img src={data.flags.svg} alt="flag" width={400}></img>}
     </div>
