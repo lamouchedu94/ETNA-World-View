529c3a9b47083c7c207e17f318cac4b940e07299
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-28 18:57:39 +0100

    [ADD] Search option

--- world-view/src/app/page.tsx
+++ world-view/src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   const [inputCountry, setinputContry] = useState<string>('')
   // const [data, setData] = useState<any>()
   const [data, setData] = useState<countryData[]>([])
-
+  const [dataStartWith, setDataStartWith] = useState<countryData[]>([])
   useEffect(() => {
     fetchAllCountryData().then((d) => {
-      data.sort((a, b) => {
+      d.sort((a, b) => {
         const nameA = a.name.common.toUpperCase()
         const nameB = b.name.common.toUpperCase()
         if (nameA < nameB) {
@@ -26,32 +26,25 @@ export default function Home() {
       // setData(data)
     })
   }, [])
-  function changeCurrentCountry() {
-    if (inputCountry !== 'null') {
-      fetchAllCountryData().then((data) => {
-        setData(data)
-        // setData(data)
-      })
-    }
-  }
 
   function handleSetInput(input: string) {
     setinputContry(input)
+    setDataStartWith(data.filter((item) => item.name.common.startsWith(inputCountry)))
   }
 
-  console.log(data)
+  // console.log(data)
   return (
     <div className="flex justify-center flex-col">
-      <div className="flex justify-center bg-[rgb(36,35,35)]">
+      <div className="flex justify-center bg-[rgb(36,35,35)] mb-10">
         <NavBar handleSetInput={handleSetInput} inputCountry={inputCountry}></NavBar>
       </div>
-      <main className="flex flex-col align-middle max-w-10">
+      {/* <main className="flex flex-col align-middle max-w-10">
         <button onClick={changeCurrentCountry}>Research</button>
-      </main>
+      </main> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
-        {data.map((elem, i) => (
-          <Carte data={elem} key={i}></Carte>
-        ))}
+        {inputCountry === ''
+          ? data.map((elem, i) => <Carte data={elem} key={i}></Carte>)
+          : dataStartWith.map((elem, i) => <Carte data={elem} key={i}></Carte>)}
       </div>
     </div>
   )
