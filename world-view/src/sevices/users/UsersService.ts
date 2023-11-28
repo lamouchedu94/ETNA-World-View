28e433bd2cc82992314229c39d7aaeeaf563458b
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-28 14:22:11 +0100

    [MOD] Api reqest

--- world-view/src/sevices/users/UsersService.ts
+++ world-view/src/sevices/users/UsersService.ts
@@ -1,9 +1,13 @@
 import { api } from '../ServiceHelper'
+import { countryData } from '@/components/country'
 
 export const fetchCountryData = async (name: string) => {
-  return await api.get('name/' + name).then((response) => response.data[0])
+  return await api.get('name/' + name).then((response) => response.data[0] as countryData)
 }
 
 export const fetchAllCountryData = async () => {
-  return await api.get('all/').then((response) => response)
+  return await api.get('all?fields=name,tld,independent,currencies,flags').then((response) => {
+    // console.log(response.data)
+    return response.data as countryData[]
+  })
 }
