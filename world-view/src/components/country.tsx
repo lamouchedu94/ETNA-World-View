31efb143003af08b7653f672ee690924f231b7ac
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-28 14:22:58 +0100

    [ADD] Carte compnent

--- world-view/src/components/country.tsx
+++ world-view/src/components/country.tsx
@@ -1,3 +1,37 @@
-export default function Carte() {
-  return <div>tg</div>
+export type countryData = {
+  name: {
+    common: string
+    official: string
+    nativeName: {
+      [key: string]: {
+        official: string
+        common: string
+      }
+    }
+  }
+  tld: {}
+  independent: boolean
+  currencies: {
+    [key: string]: {
+      name: string
+      symbol: string
+    }
+  }
+  flags: {
+    png: string
+    svg: string
+  }
+}
+
+interface CarteProps {
+  data: countryData | null
+}
+
+export function Carte({ data }: CarteProps) {
+  return (
+    <div>
+      {data && <p>{data.name.common}</p>}
+      {data && <img src={data.flags.svg} alt="flag" width={600}></img>}
+    </div>
+  )
 }
