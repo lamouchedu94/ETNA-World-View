94c56770c2ce890dad12a8df5faee480663cf8c3
lamouchedu94 <lamouchedu54@gmail.com>
2023-11-28 12:53:52 +0100

    [ADD] endpoint

--- world-view/src/sevices/users/UsersService.ts
+++ world-view/src/sevices/users/UsersService.ts
@@ -3,3 +3,7 @@ import { api } from '../ServiceHelper'
 export const fetchCountryData = async (name: string) => {
   return await api.get('name/' + name).then((response) => response.data[0])
 }
+
+export const fetchAllCountryData = async () => {
+  return await api.get('all/').then((response) => response)
+}
