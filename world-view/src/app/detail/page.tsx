5d4098b13dc0f1a29b0140cf5ebd5fea756dc6e6
lamouchedu94 <lamouchedu54@gmail.com>
2023-12-11 01:56:31 +0100

    [ADD] Start of detail page

--- world-view/src/app/detail/page.tsx
+++ world-view/src/app/detail/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { fetchOneCountryData } from '@/sevices/users/CountryService'
+import { useEffect, useState } from 'react'
+import { countryData } from '@/components/country'
 
 export default function mainDetailPage() {
+  const [ccn3, setccn3] = useState<string | null>('')
+  const [data, setData] = useState<countryData>()
   const searchParams = useSearchParams()
-
-  const search = searchParams.get('ccn3')
+  useEffect(() => {
+    setccn3(searchParams.get('ccn3'))
+  }, [])
+  useEffect(() => {
+    if (ccn3 !== null) {
+      fetchOneCountryData(ccn3).then((d) => {
+        setData(d)
+      })
+    }
+  }, [ccn3])
 
   // URL -> `/dashboard?search=my-project`
   // `search` -> 'my-project'
-  return <>Search: {search}</>
+  return <div>{data && <img src={data.flags.svg} alt="flag" width={400}></img>}</div>
 }
