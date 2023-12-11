ac1923865d6325170e0f31bcd91e1f2cf69b05c1
lamouchedu94 <lamouchedu54@gmail.com>
2023-12-11 01:29:20 +0100

    [ADD] Display CCN3 number

--- world-view/src/app/detail/page.tsx
+++ world-view/src/app/detail/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
 export default function mainDetailPage() {
-  return <div>zeroirh</div>
+  const searchParams = useSearchParams()
+
+  const search = searchParams.get('ccn3')
+
+  // URL -> `/dashboard?search=my-project`
+  // `search` -> 'my-project'
+  return <>Search: {search}</>
 }
