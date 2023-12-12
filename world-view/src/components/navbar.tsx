eb2959c6f9718f4c82fa33cecf92ac1b93c8ebe6
lamouchedu94 <lamouchedu54@gmail.com>
2023-12-12 09:15:12 +0100

    .

--- world-view/src/components/navbar.tsx
+++ world-view/src/components/navbar.tsx
@@ -5,7 +5,7 @@ interface NavBarProps {
 
 export default function NavBar({ inputCountry, handleSetInput }: NavBarProps) {
   return (
-    <nav>
+    <nav className="py-2">
       <input
         type="text"
         className="bg-[rgb(229,229,229)] rounded-2xl pl-2 py-1 m-1 border-2"
